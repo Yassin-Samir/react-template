6f5a4df695d0f6cacd99186837586209b280c634
yassin samir <yassinsam3@gmail.com>
2022-09-30 20:14:41 +0200

    adding a new file

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Section5 from "./components/section5";
 import Section6 from "./components/section6";
 import Section7 from "./components/section7";
 import Section8 from "./components/section8";
+import Section9 from "./components/section9";
 import Footer from "./components/footer";
 function App() {
   return (
@@ -17,10 +18,10 @@ function App() {
       <Section2 />
       <Section3 />
       <Section4 />
-      <Section5 />
       <Section6 />
       <Section7 />
       <Section8 />
+      <Section9 />
       <Footer />
     </>
   );
--- src/components/section8.jsx
+++ src/components/section8.jsx
@@ -1,17 +1,79 @@
-import "../css/section8.css";
+import "../css/section7.css";
+import first from "../assets/rating/testimonial-1.jpg";
 function section8() {
+  const Arr = () => (
+    <>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 576 512"
+        className="icon1"
+      >
+        <path
+          fill="gold"
+          d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"
+        />
+      </svg>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 576 512"
+        className="icon1"
+      >
+        <path
+          fill="gold"
+          d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"
+        />
+      </svg>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 576 512"
+        className="icon1"
+      >
+        <path
+          fill="gold"
+          d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"
+        />
+      </svg>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 576 512"
+        className="icon1"
+      >
+        <path
+          fill="gold"
+          d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"
+        />
+      </svg>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 576 512"
+        className="icon1"
+      >
+        <path
+          fill="gold"
+          d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"
+        />
+      </svg>
+    </>
+  );
+  const Com = (props) => (
+    <div className="rating">
+      <p className="stars">
+        <Arr />
+      </p>
+      <p className="under-starts">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. enim ad minim
+        veniam, quis nostrud exercitation.
+      </p>
+      <img src={props.src} className="profile" />
+      <p className="name">{props.name}</p>
+      <p className="title">{props.title}</p>
+    </div>
+  );
   return (
-    <div className="section8">
-      <div className="child">
-        <h1>Have a project in mind?</h1>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud consectetur
-        </p>
-        <button>Contact Us</button>
-      </div>
+    <div className="section7">
+      <Com src={first} name="Stanley Tate" title="Web Dev" />
     </div>
   );
 }
-export default section8;
+export default section7;
